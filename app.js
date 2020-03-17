15f495b276a22c4053b5fbf0580eab1a2aeac259
winwin2011 <yjh2011@live.com>
2020-03-17 20:03:46 +0800

    add main logic

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
-
-const index = require('./routes/index')
-const users = require('./routes/users')
+const path = require('path')
 
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(require('koa-static')(path.join(__dirname, '/public')))
 
 // logger
 app.use(async (ctx, next) => {
@@ -32,13 +25,14 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+// hexo-editor-server
+require('@winwin/hexo-editor-server')(app, {
+  base: process.env.HEXO_SERVER_BASE
+})
 
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
-});
+})
 
 module.exports = app
