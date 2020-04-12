84c92090a56a4d34e0e589b76c8559ec13c1c5ab
winwin2011 <yjh2011@live.com>
2020-04-13 02:07:26 +0800

    add update logic framework

--- install.js
+++ install.js
@@ -2,73 +2,77 @@ const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
+// const git = require('simple-git')(process.cwd())
 const config = require('./src/loadConfig')
 const { exec } = require('child_process')
 const logger = require('debug')('hexo-editor:installer')
 
-logger('start')
+console.clear()
 inquirer
-  .prompt([{
-    name: 'hexoRoot',
-    message: 'What\'s your hexo blog path? ' + chalk.blue('The same path as your hexo _config.yml file\n'),
-    prefix: chalk.blue('?'),
-    default: config.hexoRoot,
-    validate (v) {
-      return checkIsBlog(v)
-    }
-  }, {
-    name: 'port',
-    message: 'Which port do you like your hexo-editor running at?',
-    default: config.port || 5777,
-    validate (v) {
-      return !isNaN(v) || `number is required ${typeof v} given`
+  .prompt([
+    {
+      type: 'confirm',
+      name: 'update',
+      message: 'Update hexo-editor?',
+      default: false
     },
-    prefix: chalk.blue('?')
-  },
-  {
-    name: 'hexoServerAddress',
-    message: 'Your hexo-editor server address?',
-    default: config.hexoServerAddress || 'http://localhost:5777',
-    prefix: chalk.blue('?')
-  },
-  {
-    name: 'jwtSecret',
-    default: config.jwtSecret || 'secret',
-    prefix: chalk.blue('?')
-  },
-  {
-    name: 'jwtExpire',
-    message: 'Access token expire time ' + chalk.blue('Eg: "2 days", "10h", "7d" '),
-    default: config.jwtExpire || '1h',
-    prefix: chalk.blue('?')
-  },
-  {
-    name: 'jwtRefresh',
-    message: 'Refresh token expire time ' + chalk.blue('Eg: "2 days", "10h", "7d" '),
-    default: config.jwtRefresh || '7d',
-    prefix: chalk.blue('?')
-  },
-  {
-    name: 'username',
-    default: config.username || 'admin',
-    prefix: chalk.blue('?')
-  },
-  {
-    type: 'password',
-    name: 'password',
-    message: 'password ' +
+    {
+      name: 'hexoRoot',
+      message: 'What\'s your hexo blog path? ' + chalk.blue('The same path as your hexo _config.yml file\n'),
+      prefix: chalk.blue('?'),
+      default: config.hexoRoot,
+      validate (v) {
+        return checkIsBlog(v)
+      }
+    }, {
+      name: 'port',
+      message: 'Which port do you like your hexo-editor running at?',
+      default: config.port || 5777,
+      validate (v) {
+        return !isNaN(v) || `number is required ${typeof v} given`
+      },
+      prefix: chalk.blue('?')
+    },
+    {
+      name: 'hexoServerAddress',
+      message: 'Your hexo-editor server address?',
+      default: config.hexoServerAddress || 'http://localhost:5777',
+      prefix: chalk.blue('?')
+    },
+    {
+      name: 'jwtSecret',
+      default: config.jwtSecret || 'secret',
+      prefix: chalk.blue('?')
+    },
+    {
+      name: 'jwtExpire',
+      message: 'Access token expire time ' + chalk.blue('Eg: "2 days", "10h", "7d" '),
+      default: config.jwtExpire || '1h',
+      prefix: chalk.blue('?')
+    },
+    {
+      name: 'jwtRefresh',
+      message: 'Refresh token expire time ' + chalk.blue('Eg: "2 days", "10h", "7d" '),
+      default: config.jwtRefresh || '7d',
+      prefix: chalk.blue('?')
+    },
+    {
+      name: 'username',
+      default: config.username || 'admin',
+      prefix: chalk.blue('?')
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'password ' +
      chalk.blue('default `' + config.password || 'admin' + '`'),
-    default: config.password || 'admin',
-    mask: '*',
-    prefix: chalk.blue('?')
-  },
-  {
-    type: 'bool',
-    name: 'update',
-    default: false
-  }
+      default: config.password || 'admin',
+      mask: '*',
+      prefix: chalk.blue('?')
+    }
   ])
   .then(answers => {
+    console.clear()
     logger('answers: %O', answers)
     const userConfig = {}
     const userConfigPath = './config.user.js'
@@ -80,15 +84,29 @@ inquirer
     }
     fs.writeFileSync(userConfigPath, 'module.exports =' + JSON.stringify(userConfig).replace(',', ',\n'))
     const p = exec('npx eslint config.user.js --fix')
-    logger('formatting user.config.js')
+    console.clear()
+    console.log(chalk.blue.bold('Saving settings'))
     p.on('close', () => {
-      console.log(chalk.green.bold('Install finished!'))
-      console.log('You can modify your config by editing ' + chalk.blue.bold('config.user.js'))
-      console.log('Run ' + chalk.blue.bold('`npm run prd`') + ' to start')
-      console.log('Run ' + chalk.blue.bold('`pm2 stop hexoeditor`') + ' to stop')
-      console.log('Run ' + chalk.blue.bold('`pm2 restart hexoeditor`') + ' to restart')
-      console.log('Have fun :p')
-      console.log(chalk.grey('NOTE. If you want to change your hexo-editor server address, you need to run this installer again.'))
+      try {
+        if (answers.update) {
+          console.log(chalk.blue.bold('Fetching updates'))
+          // git.pull()
+        }
+        // git.reset('hard')
+        console.log(chalk.blue.bold('Installing...'))
+        // replace address
+        console.clear()
+        console.log(chalk.green.bold('Finished!'))
+        console.log('You can modify your config by editing ' + chalk.blue.bold('config.user.js'))
+        console.log('Run ' + chalk.blue.bold('`npm run prd`') + ' to start')
+        console.log('Run ' + chalk.blue.bold('`pm2 stop hexoeditor`') + ' to stop')
+        console.log('Run ' + chalk.blue.bold('`pm2 restart hexoeditor`') + ' to restart')
+        console.log('Have fun :p')
+        console.log(chalk.grey('NOTE. If you want to change your hexo-editor server address, you need to run this installer again.'))
+      } catch (err) {
+        console.error(chalk.bgRed.white.bold('Install failed'))
+        console.error(err)
+      }
     })
   })
   .catch(error => {
