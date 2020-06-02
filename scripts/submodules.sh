609cd44c1f51222908a59d9fedb099a732496158
winwin <>
2020-06-02 12:31:42 +0800

    remove submodule auto update

--- scripts/submodules.sh
+++ scripts/submodules.sh
@@ -1,9 +1,3 @@
 printTitle "⚙ Setup submodules"
 git submodule sync
 git submodule update --init --recursive
-git submodule foreach 'git pull origin master'
-if ! [ -f "$freshInsall" ]; then
-  printNormal "Fresh install"
-  git submodule foreach 'git pull origin master'
-  touch $freshInsall
-fi
