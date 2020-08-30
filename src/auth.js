620e7576d6334cca4cd734fca8d0909735fc59b3
winwin <>
2020-08-31 03:08:06 +0800

    add apikey

--- src/auth.js
+++ src/auth.js
@@ -8,6 +8,88 @@ if (!fs.existsSync('./data/'))fs.mkdirSync('./data')
 const db = new JSONdb('./data/db.json')
 db.set('name', config.username)
 db.set('pass', config.password)
+const debug = require('debug')('hexo-editor:server')
+
+function resolveAuthorizationHeader (ctx) {
+  if (!ctx.header || !ctx.header.authorization) {
+    return
+  }
+
+  const parts = ctx.header.authorization.split(' ')
+
+  if (parts.length === 2) {
+    const scheme = parts[0]
+    const credentials = parts[1]
+
+    if (/^Bearer$/i.test(scheme)) {
+      return credentials
+    }
+  }
+  ctx.throw(401, 'Bad Authorization header format. Format is "Authorization: Bearer <token>"')
+}
+
+exports.apiKeyAuth = async function (ctx, next) {
+  const apikey = resolveAuthorizationHeader(ctx)
+  let err
+  if (!apikey) {
+    err = new Error()
+    err.status = 401
+    err.message = 'APIKEY required'
+  } else {
+    const availableAPIKEYS = db.get('apikeys') || []
+    if (availableAPIKEYS.includes(apikey)) {
+      debug('apikey auth pass')
+      ctx.state.apikey = apikey
+      await next()
+    } else {
+      debug('APIKEY not available')
+      err = new Error()
+      err.status = 401
+      err.message = 'Authentication Error'
+    }
+  }
+  if (err) {
+    debug('apikey auth fail')
+    throw err
+  }
+}
+
+exports.requestAPIKEY = async function (ctx, next) {
+  ctx.body = {
+    success: true,
+    data: {
+      token: jwt.sign({ issueat: new Date().valueOf(), type: 'apikeytoken' }, config.apikeySecret, { expiresIn: '5min' })
+    }
+  }
+}
+
+exports.removeAPIKEY = async function (ctx, next) {
+  const apikey = ctx.state.apikey ? ctx.state.apikey : ctx.body.apikey
+  const apikeys = db.get('apikeys') || []
+  apikeys.splice(apikeys.indexOf(apikey), 1)
+  db.set('apikeys', apikeys)
+  db.sync()
+  ctx.body = {
+    success: true
+  }
+}
+
+exports.getAPIKEY = async function (ctx, next) {
+  // 这个apikey只是一个随机字符串没啥含义
+  const apikey = jwt.sign({ issueat: new Date().valueOf(), type: 'apikey' }, 's' + Math.random())
+  const apikeys = db.get('apikeys') || []
+  apikeys.push(apikey)
+  db.set('apikeys', apikeys)
+  db.sync()
+  ctx.body = {
+    success: true,
+    data: {
+      apikey
+    }
+  }
+}
+
+exports.apiKeyJwtAuth = koaJwt({ secret: config.apikeySecret })
 
 exports.jwtAuth = koaJwt({ secret: config.jwtSecret })
 // after jwtAuth payload is set inside ctx.state.user
--- src/token.js
+++ src/token.js
@@ -1,10 +1,22 @@
 const router = require('koa-router')()
 const auth = require('./auth')
+const compose = require('koa-compose')
 
 router.prefix('/token')
 
 router.get('/', auth.basicAuth, auth.getToken)
 
 router.get('/refresh', auth.jwtAuth, auth.requestRefreshToken, auth.getToken)
+
+router.post('/apikey', auth.jwtAuth, auth.requestAPIKEY)
+
+router.delete('/apikey', require('./lib/koa-parallel')([{
+  fn: auth.apiKeyAuth,
+  validator: err => err.status === 401
+}, {
+  fn: compose([auth.jwtAuth, auth.requestAccessToken])
+}]), auth.removeAPIKEY)
+
+router.get('/apikey', auth.apiKeyJwtAuth, auth.getAPIKEY)
 
 module.exports = router
