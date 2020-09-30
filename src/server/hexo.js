013e335e5903864e50317869d3ad63dcfce46268
winwin_2011 <>
2020-10-01 03:24:25 +0800

    enh(deploy):使用同步函数

--- src/server/hexo.js
+++ src/server/hexo.js
@@ -74,7 +74,7 @@ class Hexo {
    * 可能的错误：null
    * @private
    */
-  async _checkCanDeploy () {
+  _checkCanDeploy () {
     logger.debug('checking blog can deploy')
     const hexoConfigYML = YAML.parse(fs.readFileSync(path.join(this.cwd, '_config.yml')).toString())
     if (hexoConfigYML.deploy) {
@@ -110,7 +110,7 @@ class Hexo {
     this._checkIsBlog(cwd)
     this.cwd = cwd
     logger.debug('set HEXO_ROOT', this.cwd)
-    await this._checkCanDeploy()
+    this._checkCanDeploy()
     this._checkIsGit()
     if (!this.isGit) {
       logger.warn(`${this.cwd} isn't a git repository`)
