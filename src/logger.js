82f027431eeb2dccbecc5928b5a79e93b8149148
winwin_2011 <yjh2011@live.com>
2021-02-09 11:16:54 +0800

    enh(logger):改进日志模块注释

--- src/logger.js
+++ src/logger.js
@@ -4,28 +4,72 @@ const fs = require('fs')
 const {
   isDev
 } = require('./utils')
-const logFolder = path.resolve(process.cwd(), 'log')
+/**
+ * 日志目录
+ */
+const LOG_FOLDER = path.resolve(process.cwd(), 'log')
 const getLogfilePath = filename => {
-  return path.resolve(logFolder, filename)
+  return path.resolve(LOG_FOLDER, filename)
 }
-if (!fs.existsSync(logFolder)) {
-  fs.mkdirSync(logFolder)
+if (!fs.existsSync(LOG_FOLDER)) {
+  fs.mkdirSync(LOG_FOLDER)
 }
+const HEXO_EDITOR_APPENDER = 'hexo-editor'
+/**
+ * 输出通道
+ */
+const appenders = {
+  default: {
+    type: 'file',
+    filename: getLogfilePath('default.log'),
+    removeColor: true
+  },
+  [HEXO_EDITOR_APPENDER]: {
+    type: 'file',
+    filename: getLogfilePath('hexo-editor.log'),
+    removeColor: true
+  },
+  console: {
+    type: 'console',
+    layout: isDev ? {
+      type: 'pattern',
+      pattern: '%[[%d{hh:mm:ss.SSS}][%c][%p]%] %m'
+    } : {
+      type: 'pattern',
+      pattern: '%[[winwin-hexo-editor][%p]%] %m'
+    }
+  }
+}
+/**
+ * 默认输出配置
+ */
 const NOCONSOLE_DEFAULT = {
-  appenders: ['default'],
+  appenders: ['default'].concat(isDev ? ['console'] : []),
   level: isDev ? 'debug' : 'info'
 }
+/**
+ * 输出分类
+ */
 const categories = {
-  'hexo-editor-server:hexo': {
-    appenders: ['hexo-editor-server'].concat(isDev ? ['console'] : []),
+  /**
+   * hexo核心输出
+   */
+  hexo: {
+    appenders: [HEXO_EDITOR_APPENDER].concat(isDev ? ['console'] : []),
     level: isDev ? 'debug' : 'info'
   },
-  'hexo-editor-server': {
-    appenders: ['hexo-editor-server'].concat(isDev ? ['console'] : []),
+  /**
+   * 应用服务输出
+   */
+  server: {
+    appenders: [HEXO_EDITOR_APPENDER].concat(isDev ? ['console'] : []),
     level: isDev ? 'debug' : 'info'
   },
+  /**
+   * http服务输出
+   */
   http: {
-    appenders: ['console', 'hexo-editor-server', 'default'],
+    appenders: ['console', HEXO_EDITOR_APPENDER, 'default'],
     level: isDev ? 'debug' : 'info'
   },
   default: {
@@ -33,32 +77,14 @@ const categories = {
     level: isDev ? 'debug' : 'info'
   }
 }
-const services = ['services:apikey-service', 'services:config-service', 'services:data-service']
+/**
+ * 各种服务，使用默认配置
+ */
+const services = []
 services.map(key => {
   categories[key] = NOCONSOLE_DEFAULT
 })
 log4js.configure({
-  appenders: {
-    default: {
-      type: 'file',
-      filename: getLogfilePath('default.log'),
-      removeColor: true
-    },
-    'hexo-editor-server': {
-      type: 'file',
-      filename: getLogfilePath('hexo-editor-server.log'),
-      removeColor: true
-    },
-    console: {
-      type: 'console',
-      layout: isDev ? {
-        type: 'pattern',
-        pattern: '%[[%d{hh:mm:ss.SSS}][%c][%p]%] %m'
-      } : {
-        type: 'pattern',
-        pattern: '%[[winwin-hexo-editor][%p]%] %m'
-      }
-    }
-  },
+  appenders,
   categories
 })
