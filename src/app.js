2048b0c25fdb8dd7b0fd2e3f38ab284f9dafb49e
winwin_2011 <yjh2011@live.com>
2021-02-09 11:03:59 +0800

    clean

--- src/app.js
+++ src/app.js
@@ -5,19 +5,8 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const koaLogger = require('koa-logger')
 const cors = require('koa-cors')
-const path = require('path')
 const log4js = require('log4js')
 const logger = log4js.getLogger('server')
-const authController = require('./auth/controller')
-const authRouter = require('./auth/router')
-const settings = require('./settings/router')
-const version = require('./version')
-const { configService } = require('./service/config_service')
-const {
-  hexoeditorserver,
-  initHexo,
-  HexoError
-} = require('./server')
 
 // error handler
 onerror(app)
@@ -37,9 +26,6 @@ app.use(async (ctx, next) => {
   }
 })
 
-const { dataServiceErrorHandler } = require('./errorHandlers')
-app.use(dataServiceErrorHandler)
-
 // cors
 app.use(cors())
 
@@ -53,39 +39,5 @@ app.use(koaLogger((str, args) => {
   // default is process.stdout(by console.log function)
   log4js.getLogger('http').info(str)
 }))
-
-// static resources
-const serveStatic = require('koa-static')
-app.use(serveStatic(path.join(process.cwd(), '/frontend/dist/pwa')))
-
-// install
-const isInstalled = configService.isInstalled()
-if (!isInstalled) {
-  const install = require('./install')
-  app.use(install.routes(), install.allowedMethods())
-} else {
-  initHexo(configService.getHexoRoot()).catch(err => {
-    if (![HexoError.EMPTY_HEXO_ROOT, HexoError.NOT_BLOG_ROOT].includes(err.code)) {
-      logger.error('Unknown Error:', err)
-      process.exit(1)
-    }
-  })
-}
-
-// hexo-editor-server
-hexoeditorserver(app, {
-  base: 'hexoeditorserver',
-  auth: authController.apikeyOrJwt
-})
-
-// routes
-app.use(authRouter.routes(), authRouter.allowedMethods())
-app.use(settings.routes(), settings.allowedMethods())
-app.use(version.routes(), version.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-})
 
 module.exports = app
